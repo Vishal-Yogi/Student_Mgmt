e198835b2542ee98f2428b9acd9dc3310de4d7e8
Vishal Yogi <007vishalyogi@gmail.com>
2025-10-27 16:11:02 +0530

    Temporarily enabled DEBUG for troubleshooting

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i-4pi@0galow%m9l8vqz-wu)ms(cdr#mq$d6=!n&mb2q=6h+ce'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = ['student-mgmt-wct7.onrender.com', 'localhost', '127.0.0.1']
 
